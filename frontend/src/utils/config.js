6b2fa2c052448788c6c3f02673f9c7f161fbe0df
Roshan Parida <roshanparida24@gmail.com>
2024-08-10 14:54:24 +0530

    added deployment adjustments

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+export const backendUrl = process.env.BACKEND_URL;
+export const cloudinary_cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+export const cloudinary_upload_preset = process.env.CLOUDINARY_UPLOAD_PRESET;
