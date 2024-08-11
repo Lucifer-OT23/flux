f23b828f5c7fecba438efba4a0967794051e9d67
Roshan Parida <roshanparida24@gmail.com>
2024-08-11 14:36:08 +0530

    fixed: configs for cloudinary

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,5 @@
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const cloudinary_cloud_name = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 export const cloudinary_upload_preset = import.meta.env
-    .VITE_CLOUDINARY_CLOUD_NAME;
-export const cloudinary_cloud_name = import.meta.env
     .VITE_CLOUDINARY_UPLOAD_PRESET;
 export const cloudinary_key = import.meta.env.VITE_CLOUDINARY_KEY;
